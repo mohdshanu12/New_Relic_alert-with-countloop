5f839ad3a0a586a2c2893c9df84b37393ae54740
mohdshanu12 <129376678+mohdshanu12@users.noreply.github.com>
2023-07-30 18:39:13 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,46 @@
+ 
+resource "newrelic_alert_policy" "foo" {
+  count = var.alert_policy_count
+
+  name = "foo-${count.index + 1}"
+}
+
+resource "newrelic_nrql_alert_condition" "foo" {
+  count = length(newrelic_alert_policy.foo)
+
+  account_id                     = 4043696
+  policy_id                      = newrelic_alert_policy.foo[count.index].id
+  type                           = "static"
+  name                           = "foo-condition-${count.index + 1}"
+  description                    = "Alert when transactions are taking too long"
+  runbook_url                    = "https://www.example.com"
+  enabled                        = true
+  violation_time_limit_seconds   = 3600
+  fill_option                    = "static"
+  fill_value                     = 1.0
+  aggregation_window             = 60
+  aggregation_method             = "event_flow"
+  aggregation_delay              = 120
+  expiration_duration            = 120
+  open_violation_on_expiration   = true
+  close_violations_on_expiration = true
+  slide_by                       = 30
+
+  nrql {
+    query = "SELECT average(duration) FROM Transaction where appName = 'Your App'"
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 5.5
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "above"
+    threshold             = 3.5
+    threshold_duration    = 600
+    threshold_occurrences = "ALL"
+  }
+}
