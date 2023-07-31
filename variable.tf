447a36b576c5abbf603aa0a34185e4f1a1f6dd76
mohdshanu12 <129376678+mohdshanu12@users.noreply.github.com>
2023-07-31 15:15:16 +0530

    Delete variable.tf

--- variable.tf
+++
@@ -1,9 +0,0 @@
-# variable.tf
-
-variable "alert_policy_count" {
-  type    = number
-  default = 3 # Change this value to set the number of alert policies you want to create.
-}
-
-// i think i don't need terraform.tfvars file because i set the defautl limit. {
-  
